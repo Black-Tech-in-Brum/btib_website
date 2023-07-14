85c98daa661fd5d1ee0b6db97f2db5612298066d
OUAKHILATOU <ouakhilatou@ouakhilatousmbp.home>
2023-07-14 16:18:16 +0100

    without untracked files

---
+++ README.md
@@ -0,0 +1,31 @@
+# BTIT LANDING PAGE
+
+# 🛎️🛎️ WELCOME TO BTIB Landing page README Folder
+
+## Description
+
+This is a ReactJS-based landing page template based on the UX team design that you can find here. <a href="https://www.figma.com/file/QhB2pdyTcwQamr8i53LnEi/BTIB-Website-project?type=design&node-id=2-432&mode=design&t=lzX2D6V3h0B3OSpK-0">UX DESIGN</a>
+In a component folder, you will find all components with a starter code.
+
+### Add BTIB data
+
+There is a JSON file in the data folder with dummy data that is subject to change based on the BTIB landing page.
+
+## StrapiServer Folder
+
+This folder is for future use.
+
+connecting Strapi and the client side of the website
+
+All 'visual' data can be easily modified by publishing directly from the Strapi user admin.
+You can learn more about Strapi here. <a href="https://strapi.io/">Strapi</a>
+
+### Clone Files
+
+After cloning the files, you will have to go to btibWebsite and run `npm develop`
+
+##### TALWIND CSS CHEAT SHEET
+
+<a href="https://tailwindcomponents.com/cheatsheet/">TalwindCSS</a>
+
+##### BTIB WEB TEAM
---
+++ src/components/JoinDiscord.jsx
@@ -0,0 +1,30 @@
+function JoinDiscord() {
+    return (
+      <section className="bg-white py-20">
+        <div className=" px-4 py-8 mx-auto">
+          <div className="place-self-center ">
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-black">
+           JOIN DISCORD
+            </h1>
+            <p className="max-w-2xl mb-6 font-light text-black-500 dark:text-black-400">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
+              dapibus leonec. Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
+            
+            </p>
+            <a
+              href="#"
+              className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+            >
+              Get started
+              
+              
+            </a>
+       
+          </div>
+        </div>
+      </section>
+    );
+  }
+  
+  export default JoinDiscord;
+  
