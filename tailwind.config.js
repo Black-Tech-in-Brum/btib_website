3cb35cede8f935c43b568bec02a1118e43b54716
akpekig <gillianakpeki@hotmail.co.uk>
2023-08-16 07:24:21 +0100

    Add Tailwind variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,39 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#00040f",
-        secondary: "#00f6ff",
-        dimWhite: "rgba(255, 255, 255, 0.7)",
-        dimBlue: "rgba(9, 151, 124, 0.1)",
+        primary: {
+          100: "#ff477a",
+          200: "#ec3668",
+        },
+        secondary: {
+          100: "#3e6fea",
+          200: "#3f0de7",
+          300: "#431ccd",
+        },
+        neutral: {
+          100: "#fbfdfb",
+          200: "#f7f5f6",
+          300: "#f3f1f2",
+          400: "#84818d",
+          500: "#161128",
+        },
+        error: "#ff3b30",
+        success: "#34c759",
+        warning: "#ffc107",
+      },
+      backgroundImage: {
+        "noise-gradient": "url('/img/noise-gradient.png')",
+      },
+      backgroundSize: {
+        "100%": "100%",
       },
       fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
+        atype: ["Atype Display", "sans-serif"],
+        satoshi: ["Satoshi", "sans-serif"],
+      },
+      fontSize: {
+        xxs: ["0.625rem", "1rem"],
+        "5/2xl": ["1.75rem", "2.25rem"],
       },
     },
     screens: {
