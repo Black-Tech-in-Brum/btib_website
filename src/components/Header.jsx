da3bb15422f9f2e7c181b0764ff6339d4f8a1ce6
OUAKHILATOU <ouakhilatou@ouakhilatousmbp.home>
2023-08-24 09:42:42 +0100

    added some changes on the datas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-// import { ReactComponent as Scribble } from "@assets/icons/scribble.svg";
+import { ReactComponent as Scribble } from "@assets/icons/scribble.svg";
 
 // import { useState } from "react";
 import datas from "../data/data.json"
@@ -9,7 +9,8 @@ function Header () {
         <header className="flex justify-center bg-blue-600 items-center h-screen bg-noise-gradient bg-secondary-300 bg-blend-hard-light bg-full text-neutral-100 pt-12 px-6 sm:px-24 pb-2">
         <div className=" p-6 ">
                 <div className="py-7 text-center">
-                <h1 className="text-4xl font-bold font-satoshi capitalize text-white "> Black Tech in Brum </h1>
+                <h1 className="text-6xl font-bold font-satoshi capitalize text-white "> Black Tech in Brum </h1>
+                <Scribble className=" h-[0.875rem] "  aria-hidden="true" ></Scribble>
                 <p className="text-xl font-satoshi text-white"> Empowering Black Techies in Birmingham to Innovate Lead, and Shape the Future. </p>
               </div>
       
