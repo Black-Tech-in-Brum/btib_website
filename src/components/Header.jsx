4266e7b315f52bd6d809817973372d4f6c69a33a
OUAKHILATOU <ouakhilatou@ouakhilatousmbp.home>
2023-08-24 09:23:07 +0100

    added pictures and titles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,34 @@
-function Header() {
-  return (
-    <div className="py-80">
-      <div className="container m-auto px-6">
-        <div className="lg:flex justify-between items-center">
-          <div className="lg:w-6/12 lg:p-0 p-7">
-            <h1 className="text-4xl font-bold leading-tight mb-5 capitalize">
-              {' '}
-              Professional Tailwind theme designed for developers.{' '}
-            </h1>
-            <p className="text-xl">
-              {' '}
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-              dapibus leonec.{' '}
-            </p>
+// import { ReactComponent as Scribble } from "@assets/icons/scribble.svg";
 
-            <div className="py-5">
-              <a
-                href="#"
-                className="text-white rounded-full py-2 px-5 text-lg font-semibold bg-yellow-600 inline-block border border-yellow-600 mr-3"
-              >
-                Try for free
-              </a>
-              <a
-                href="#"
-                className="text-black rounded-full py-2 px-5 text-lg font-semibold bg-gray-400 inline-block border hover:bg-white hover:text-black"
-              >
-                Requist a demo
-              </a>
-            </div>
+// import { useState } from "react";
+import datas from "../data/data.json"
+
+function Header () {
+  // const [items] = useState(data);
+    return (
+        <header className="flex justify-center bg-blue-600 items-center h-screen bg-noise-gradient bg-secondary-300 bg-blend-hard-light bg-full text-neutral-100 pt-12 px-6 sm:px-24 pb-2">
+        <div className=" p-6 ">
+                <div className="py-7 text-center">
+                <h1 className="text-4xl font-bold font-satoshi capitalize text-white "> Black Tech in Brum </h1>
+                <p className="text-xl font-satoshi text-white"> Empowering Black Techies in Birmingham to Innovate Lead, and Shape the Future. </p>
+              </div>
+      
+        <div className="grid grid-cols-5 gap-4"> 
+        { datas && datas.map(({picture1,id}) => (
+          <div key={id} className="w-full h-auto mt-10 block rounded-lg ">    
+           <img src={picture1} className=""/>
           </div>
+
+        ))
+      }
+          </div>
+          <button className=" mt-10 block rounded-lg bg-red-600 px-4 py-3 font-satoshi font-bold">    
+                   <a href="#" className="">Learn More</a>
+              </button>
         </div>
-      </div>
-    </div>
-  )
+      </header>
+    
+    )
 }
 
-export default Header
+export default Header;
