e9afd8cfed7d538041022a6d1e4a43d8d916f09f
OUAKHILATOU <ouakhilatou@ouakhilatousmbp.home>
2023-08-26 23:10:50 +0100

    colour modified, grid size redifined

--- src/components/Membership.jsx
+++ src/components/Membership.jsx
@@ -15,18 +15,18 @@ function Header () {
                 <br/>experience levels are welcomed and embraced </p>
               </div>
       
-        <div className="container overflow-auto mx-auto gap-6 grid grid-cols-[400px_400px_400px_400px_400px_400px] p-12 "> 
+        <div className="container overflow-y-scroll no-scrollbar mx-auto gap-6 grid grid-cols-[400px_400px_400px_400px_400px_400px] p-12  "> 
         { datas && datas.map(({benefitDesc,membersBenefit,id}) => (
-          <div key={id} className="h-90 bg-gray-100 rounded-3xl">   
-           <h1  className="text-center text-xl font-satoshi font-bold"> {membersBenefit}</h1>
-           <p className="text-center font-satoshi ">{benefitDesc}</p>
+          <div key={id} className="bg-gray-100 rounded-3xl py-6 hover:bg-red-500 hover:text-white">   
+           <h1  className="text-center text-2xl font-satoshi font-bold py-4"> {membersBenefit}</h1>
+           <p className="text-center font-satoshi text-xl pb-9 px-6">{benefitDesc}</p>
           </div>
 
         ))
       }
           </div>
           <div className="flex items-center justify-center">
-          <button className=" mt-10 block rounded-lg bg-red-600 px-12 py-3 font-satoshi font-bold text-white">    
+          <button className=" mt-10 block rounded-lg bg-red-500 px-12 py-3 font-satoshi font-bold text-white">    
                    <a href="#" className="">Become a member</a>
               </button>
           </div>
