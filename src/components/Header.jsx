c784e1b3d93e47e2b1b666487293ba6e55e060d8
OUAKHILATOU <ouakhilatou@ouakhilatousmbp.home>
2023-08-26 22:05:50 +0100

    resized images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,7 @@ function Header () {
                 <br/>Lead, and Shape the Future. </p>
               </div>
       
-        <div className="container gap-2 grid grid-cols-[400px_400px_500px_400px_400px] p-12 overflow-auto"> 
+        <div className="md:containermd:overflow-hidden pr-20 pl-20 gap-2 grid grid-cols-[320px_350px_400px_350px_320px] p-12  "> 
         { datas && datas.map(({picture1,id}) => (
           <div key={id} className="">   
            <img src={picture1} className="rounded-3xl overflow-hidden"/>
