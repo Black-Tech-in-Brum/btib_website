dc0b2889384fa75e951ca98fc704bc47b5f37c47
OUAKHILATOU <ouakhilatou@ouakhilatousmbp.home>
2023-09-01 19:05:33 +0100

    add some change to the picture size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,9 @@ import datas from "../data/data.json"
 function Header () {
   // const [items] = useState(data);
     return (
-        <header className="flex justify-center bg-blue-600 items-center h-screen bg-noise-gradient bg-secondary-300 bg-blend-hard-light bg-full text-neutral-100 pt-12 px-6 sm:px-24 pb-2">
-        <div className=" p-6 ">
-                <div className="text-center p-11">
+        <header className="flex justify-center bg-blue-600 items-center w-screen bg-noise-gradient bg-secondary-300 bg-blend-hard-light bg-full text-neutral-100 pt-12 pb-2">
+        <div className="">
+                <div className="text-center">
                 <h1 className="text-6xl font-bold font-satoshi capitalize text-white"> Black Tech in Brum </h1>
                 <div className="flex items-center justify-center ">
                 <Scribble className="w-181"/>
@@ -17,7 +17,7 @@ function Header () {
                 <br/>Lead, and Shape the Future. </p>
               </div>
       
-        <div className="md:containermd:overflow-hidden pr-20 pl-20 gap-2 grid grid-cols-[320px_350px_400px_350px_320px] p-12  "> 
+        <div className="container overflow-auto py-12 py-20 gap-2 grid grid-cols-[320px_350px_400px_350px_320px] py-12 "> 
         { datas && datas.map(({picture1,id}) => (
           <div key={id} className="">   
            <img src={picture1} className="rounded-3xl overflow-hidden"/>
